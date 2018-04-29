b2f8cab498c6825b667ea452d32bc4bd6d97fe8d
靳玉亮 <leo@air.local>
2018-04-28 20:32:49 -0500

    add RestTemplate bean config

--- src/main/java/com/yuliang/myapp/MyappApplication.java
+++ src/main/java/com/yuliang/myapp/MyappApplication.java
@@ -2,9 +2,16 @@ package com.yuliang.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MyappApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyappApplication.class, args);
